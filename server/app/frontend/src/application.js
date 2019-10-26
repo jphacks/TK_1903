3f6a4ca2c396dc6e895b2a11cd7575274ef23678
arsley <blue20will@gmail.com>
2019-10-26 23:53:39 +0900

    :wrench: Fix dev-server errors

--- server/app/frontend/src/application.js
+++ server/app/frontend/src/application.js
@@ -1,7 +1,7 @@
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
-require("channels");
+require("../channels");
 
 // CSS entrypoint
 import '../styles/application.scss';
